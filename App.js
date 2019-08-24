c2e5742b79ced9e0c16ea2ccc7b7beb5ffc04818
Isgi Arriza <isgiarriza@gmail.com>
2019-08-24 15:52:46 +0700

    redux starter

---
+++ App.js
@@ -0,0 +1,3 @@
+import Welcome from './src/screens/Welcome';
+
+export default Welcome;
---
+++ src/screens/Welcome.js
@@ -0,0 +1,116 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React, {Fragment} from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+  Button
+} from 'react-native';
+
+import {
+  LearnMoreLinks,
+  Colors,
+  DebugInstructions,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+import { createStore } from 'redux';
+
+import Header from '../components/Header';
+
+const initialState = {
+  number: 0
+}
+// this reducer
+function counter(state = initialState, action) {
+  switch (action.type) {
+    case 'INCREMENT':
+      return {
+        number: state.number + 1
+      }
+    case 'DECREMENT':
+      return {
+        number: state.number - 1
+      }
+    default:
+      return state
+  }
+}
+
+let store = createStore(counter)
+
+store.subscribe(() => console.log(store.getState().number))
+
+// this action
+const actionINC = () => {
+  return {
+    type: 'INCREMENT'
+  }
+}
+
+const Welcome = () => {
+  return (
+    <Fragment>
+      <StatusBar barStyle="dark-content" />
+      <SafeAreaView>
+        <ScrollView
+          contentInsetAdjustmentBehavior="automatic"
+          style={styles.scrollView}>
+          <Header />
+          <Text>{}</Text>
+          <Button title="INC" onPress={() => store.dispatch(actionINC())} />
+        </ScrollView>
+      </SafeAreaView>
+    </Fragment>
+  );
+};
+
+const styles = StyleSheet.create({
+  scrollView: {
+    backgroundColor: Colors.lighter,
+  },
+  engine: {
+    position: 'absolute',
+    right: 0,
+  },
+  body: {
+    backgroundColor: Colors.white,
+  },
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+    color: Colors.black,
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+    color: Colors.dark,
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+  footer: {
+    color: Colors.dark,
+    fontSize: 12,
+    fontWeight: '600',
+    padding: 4,
+    paddingRight: 12,
+    textAlign: 'right',
+  },
+});
+
+export default Welcome;
